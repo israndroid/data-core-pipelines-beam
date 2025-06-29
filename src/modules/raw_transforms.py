ec5f62acc47f742c806e8c40bb478ead8ffac82e
israndroid <135785253+israndroid@users.noreply.github.com>
2025-06-28 23:16:13 -0400

    Working native reading-writing io apache beam, install pip install apache_beam[dataframe] requiriments, for use datagrame API at Apache Beam

--- src/modules/raw_transforms.py
+++ src/modules/raw_transforms.py
@@ -1,6 +1,18 @@
 import apache_beam as beam
 
 
+class RowToDict(beam.PTransform):
+    """
+    A Beam PTransform that converts each row of a PCollection into a dictionary.
+    This transform is used to standardize the format of the data in the PCollection.
+    """
+    def __init__(self):
+        super().__init__()
+    
+    def expand(self, pcoll):
+        return (pcoll
+                | 'Convert Row to Dict' >> beam.Map(lambda x: dict(x)))
+
 class StandarizeHeader(beam.PTransform):
     """
     A Beam PTransform that standardizes the header of a PCollection.
